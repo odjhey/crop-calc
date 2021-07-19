9c05d093e2bbe583edac202e0eaa18bf1b7cc5c3
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-07-20 06:26:46 +0800

    export bayads

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,7 +1,10 @@
 export { createCrop } from "./xdomains/Crop.ts";
 export { addExpense, createExpense } from "./xdomains/Expense.ts";
+export { addBayad, createBayad } from "./xdomains/Bayad.ts";
 export { sum } from "./xdomains/Operations.ts";
 export { computeCrop } from "./usecases/computeCrop.ts";
 
-const pong = () => {return `pong 123`}
-export {pong}
+const pong = () => {
+  return `pong 123`;
+};
+export { pong };
