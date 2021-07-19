18e4c7a0ef460e49fdab10d962be6042188c1791
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-07-20 05:06:56 +0800

    add pong shared

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -2,3 +2,6 @@ export { createCrop } from "./xdomains/Crop.ts";
 export { addExpense, createExpense } from "./xdomains/Expense.ts";
 export { sum } from "./xdomains/Operations.ts";
 export { computeCrop } from "./usecases/computeCrop.ts";
+
+const pong = () => {return `pong 123`}
+export {pong}
