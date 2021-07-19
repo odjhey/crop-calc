088cfc1dc4de98c45b3bb231456c20e555cc48d0
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-07-19 12:15:48 +0800

    add react-hook-form

--- webapp/components/ExpenseForm.tsx
+++ webapp/components/ExpenseForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, FormControl, FormLabel, Input, Select } from "@vechaiui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 function StyledFormControl(props) {
   return <FormControl className={`py-1 px-1 ${props.className}`}>
@@ -7,86 +8,78 @@ function StyledFormControl(props) {
   </FormControl>;
 }
 
-export default function ExpenseForm(props) {
-  const [input, setInput] = useState({
-    description: "",
-    qty: 0,
-    price: 0,
-    curr: "",
-  });
+interface IFormInput {
+  description: string;
+  qty: number;
+  price: number;
+  curr: string;
+}
 
-  const onSubmit = () => {
-    props.onSubmit(input);
+export default function ExpenseForm(props) {
+  const { register, handleSubmit, formState: { errors } } = useForm<
+    IFormInput
+  >();
+
+  const onSubmit = (data) => {
+    props.onSubmit(data);
   };
 
   return (
-    <div className="flex flex-wrap w-full p-8">
-      <StyledFormControl className={`w-80`}>
-        <FormLabel htmlFor="description" id="description-label">
-          Description
-        </FormLabel>
-        <Input
-          id="description"
-          type="text"
-          onChange={(e) => {
-            setInput(() => {
-              return { ...input, description: e.target.value };
-            });
-          }}
-        />
-      </StyledFormControl>
-      <StyledFormControl className={`w-40`}>
-        <FormLabel htmlFor="qty" id="qty-label">
-          Qty
-        </FormLabel>
-        <Input
-          id="qty"
-          type="number"
-          onChange={(e) => {
-            setInput(() => {
-              return { ...input, qty: e.target.value };
-            });
-          }}
-        />
-      </StyledFormControl>
-      <StyledFormControl className={`w-40`}>
-        <FormLabel htmlFor="unit-price" id="unit-price-label">
-          Unit Price
-        </FormLabel>
-        <Input
-          id="unit-price"
-          type="number"
-          onChange={(e) => {
-            setInput(() => {
-              return { ...input, price: e.target.value };
-            });
-          }}
-        />
-      </StyledFormControl>
-      <StyledFormControl className={`w-40`}>
-        <FormLabel htmlFor="currency" id="currency-label">
-          Currency
-        </FormLabel>
-        <Select
-        id="currency"
-          value={input.curr}
-          onChange={(e) => {
-            setInput(() => {
-              return { ...input, curr: e.target.value };
-            });
-          }}
-        >
-          <option value="PHP">PHP</option>
-          <option value="USD">USD</option>
-          <option value="JPY">JPY</option>
-          <option value="KRW">KRW</option>
-        </Select>
-      </StyledFormControl>
-      <StyledFormControl className={`py-2`}>
-        <Button variant="solid" color="primary" onClick={onSubmit}>
-          Button
-        </Button>
-      </StyledFormControl>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-wrap w-full p-8">
+        <StyledFormControl className={`w-80`}>
+          <FormLabel htmlFor="description" id="description-label">
+            Description
+          </FormLabel>
+          <Input
+            invalid={errors.description ? true : false}
+            id="description"
+            type="text"
+            {...register("description", { required: true, maxLength: 20 })}
+          />
+        </StyledFormControl>
+        <StyledFormControl className={`w-40`}>
+          <FormLabel htmlFor="qty" id="qty-label">
+            Qty
+          </FormLabel>
+          <Input
+            invalid={errors.qty ? true : false}
+            id="qty"
+            type="number"
+            {...register("qty", { required: true, min: 0, max: 9999 })}
+          />
+        </StyledFormControl>
+        <StyledFormControl className={`w-40`}>
+          <FormLabel htmlFor="unit-price" id="unit-price-label">
+            Unit Price
+          </FormLabel>
+          <Input
+            invalid={errors.price ? true : false}
+            id="unit-price"
+            type="number"
+            {...register("price", { required: true, min: 0, max: 99999 })}
+          />
+        </StyledFormControl>
+        <StyledFormControl className={`w-40`}>
+          <FormLabel htmlFor="currency" id="currency-label">
+            Currency
+          </FormLabel>
+          <Select
+            id="currency"
+            {...register("curr", { required: true })}
+          >
+            <option value="PHP">PHP</option>
+            <option value="USD">USD</option>
+            <option value="JPY">JPY</option>
+            <option value="KRW">KRW</option>
+          </Select>
+        </StyledFormControl>
+        <StyledFormControl className={`py-2`}>
+          <Button variant="solid" color="primary" type="submit">
+            Button
+          </Button>
+        </StyledFormControl>
+      </div>
+    </form>
   );
 }
